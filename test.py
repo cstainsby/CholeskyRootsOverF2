d7d5661dfe6c83823bad5044cbf4b71251721564
Cole Stainsby <cstainsby@zagmail.gonzaga.edu>
2022-02-07 23:21:04 +0000

    pass all sqrt root tests other than 1x1

--- test.py
+++ test.py
@@ -35,7 +35,7 @@ def test_one_by_one_matrix():
 def test_two_by_two_matrix_cholesky():
     """test the two by two upper trianglar matrix 
     test generation of all cholesky roots of 2x2
-"""
+    """
     # general matrix
     upper_triangular_general_matrix_2x2 = np.array([[und, und],
                                                     [0,   und]], dtype="int16")
@@ -59,7 +59,7 @@ def test_two_by_two_matrix_cholesky():
 
     compare_test_and_result_lists_utility(result_list=cholesky_list, test_list=test_list)
 
-def test_three_by_three_matrix():
+def test_three_by_three_matrix_cholesky():
     """test the three by three upper triangular matrix 
     test generation of all cholesky roots of 3x3
     """
@@ -91,6 +91,65 @@ def test_three_by_three_matrix():
 
     compare_test_and_result_lists_utility(result_list=cholesky_list, test_list=test_list)
 
+
+def test_two_by_two_matrix_sqrt():
+    """test the two by two upper trianglar matrix 
+    test generation of all cholesky roots of 2x2
+    """
+    # general matrix
+    upper_triangular_general_matrix_2x2 = np.array([[und, und],
+                                                    [0,   und]], dtype="int16")
+
+    # list that spans all possibilities from general matrix
+    upper_triangular_list_2x2 = main.find_matrix_list_from_gen(GF, upper_triangular_general_matrix_2x2)
+
+    print("generated lists")
+    for i, list in enumerate(upper_triangular_list_2x2):
+        main.pretty_print_numpy_array(list, "list #" + str(i + 1), with_index_labels= False, with_shape=False)
+
+    sqrt_list = main.generate_square_root_matrices(GF, upper_triangular_list_2x2)
+
+
+    test_list = [np.array([[0, 0], [0, 0]]),
+                np.array([[0, 1], [0, 0]])]
+
+    print("test list")
+    for i, list in enumerate(test_list):
+        main.pretty_print_numpy_array(list, "list #" + str(i + 1), with_index_labels= False, with_shape=False)
+
+    compare_test_and_result_lists_utility(result_list=sqrt_list, test_list=test_list)
+
+def test_three_by_three_sqrt():
+    """test the three by three upper triangular matrix 
+    test generation of all cholesky roots of 3x3
+    """
+    # general matrix 
+    upper_triangular_general_matrix_3x3 = np.array([[und, und, und],
+                                                    [0,   und, und],
+                                                    [0,   0,   und]], dtype="int16")
+
+    # list that spans all possibilities from general matrix
+    upper_triangular_list_3x3 = main.find_matrix_list_from_gen(GF, upper_triangular_general_matrix_3x3)
+
+    print("generated lists")
+    for i, list in enumerate(upper_triangular_list_3x3):
+        main.pretty_print_numpy_array(list, "list #" + str(i + 1), with_index_labels= False, with_shape=False)
+
+    sqrt_list = main.generate_square_root_matrices(GF, upper_triangular_list_3x3)
+
+
+    test_list = [np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+                np.array([[0, 0, 0], [0, 0, 1], [0, 0, 0]]),
+                np.array([[0, 0, 1], [0, 0, 0], [0, 0, 0]]),
+                np.array([[0, 0, 1], [0, 0, 1], [0, 0, 0]]),
+                np.array([[0, 1, 0], [0, 0, 0], [0, 0, 0]]),
+                np.array([[0, 1, 1], [0, 0, 0], [0, 0, 0]])]
+    
+    print("test list")
+    for i, list in enumerate(test_list):
+        main.pretty_print_numpy_array(list, "list #" + str(i + 1), with_index_labels= False, with_shape=False)
+
+    compare_test_and_result_lists_utility(result_list=sqrt_list, test_list=test_list)
 
 def compare_test_and_result_lists_utility(result_list, test_list):
     assert len(result_list) == len(test_list)
