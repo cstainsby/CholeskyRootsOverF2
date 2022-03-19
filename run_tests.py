2891ef73ca9310d52f446df7b3cc6635cf7d27fd
Cole Stainsby <cstainsby@zagmail.gonzaga.edu>
2022-03-19 16:49:17 -0700

    update simple generator's docstrings and main function(now conforms to trees run settings)

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-import test_simpleCholSqrtGenerator
-import test_treeCholSqrtGenerator
-
-"""
-This file is meant to be used for running each test I have created
-"""
--- simpleCholSqrtGenerator.py
+++ simpleCholSqrtGenerator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 import utils
 
@@ -6,15 +7,16 @@ import utils
 # placeholder for an undetermined valu
 UNDEFINED = -1
 
-
-def main(output_location, GF, n):
-    """
-    DESC:
-    driver for program
-    """
-    pass
-
 def generate_list_of_type_matrices(GF, n, type):
+    """
+    DESC:                                                                            
+    generate a list of matrices which conform to runtype set, GF, and n       
+
+    PARAMS                                                                           
+    GF(int)                                                                          
+    matrix_list(list of np matrices)
+    
+    """
     general_upper_matrix = utils.generate_upper_triangular_matrix_of_nxn(n)
     upper_triangular_list = find_matrix_list_from_gen(GF, general_upper_matrix)
 
@@ -33,13 +35,18 @@ def generate_list_of_type_matrices(GF, n, type):
 
 def generate_sqrt_matrices(GF, matrix_list):
     """
-    DESC:                                                                            \n
-    given a list of matrices, find the ones that obey the type function              \n
-    A matrix is a square root if a matrix M's (M * M) == zero matrix                 \n
-    PARAMS                                                                           \n
-    GF(int)                                                                          \n
-    matrix_list(list of np matrices)                                                 \n
-    INFO
+    DESC:                                                                            
+    given a list of matrices, find the ones that obey the type function              
+    A matrix is a square root if a matrix M's (M * M) == zero matrix        
+
+    PARAMS:                                                                     
+    GF(int)                                                                          
+    matrix_list(list of np matrices)      
+
+    RETURN:
+    type_matrix_list(list of matrix of int): all conform to sqrt
+
+    INFO:
     this can generate both chols and sqrts, just pass in the utils helper function to sort them out
     """
     type_matrix_list = []       
@@ -53,12 +60,17 @@ def generate_sqrt_matrices(GF, matrix_list):
 
 def generate_chol_matrices(GF, matrix_list):
     """
-    DESC:                                                                            \n
-    given a list of matrices, find the ones that obey the type function              \n
-    A matrix is a square root if a matrix M's (M * M) == zero matrix                 \n
-    PARAMS                                                                           \n
-    GF(int)                                                                          \n
-    matrix_list(list of np matrices)                                                 \n
+    DESC:                                                                            
+    given a list of matrices, find the ones that obey the type function              
+    A matrix is a square root if a matrix M's (M * M) == zero matrix  
+
+    PARAMS                                                                           
+    GF(int)                                                                          
+    matrix_list(list of np matrices)   
+
+    RETURN:
+    type_matrix_list(list of matrix of int): all conform to chol
+
     INFO
     this can generate both chols and sqrts, just pass in the utils helper function to sort them out
     """
@@ -73,11 +85,15 @@ def generate_chol_matrices(GF, matrix_list):
 
 def find_matrix_list_from_gen(GF, gen_matrix):
     """
-    DESC:                                                                           \n
-    base function for finding all permutations of a generalized matrix              \n
+    DESC:                                                                           
+    base function for finding all permutations of a generalized matrix 
+
     PARAMS:
-    flat_matrix: general matrix that has been flattened                             \n
-    GF:          The field which our general matrix is allowed to span              \n
+    gen_matrix(matrix of int): general matrix for all possible matrices in final list      
+    GF: The field which our general matrix is allowed to span 
+
+    RETURN:
+    result_list(list of matrix of int): list of selected matrices            
     """
     flat_gen_matrix = gen_matrix.flatten()
 
@@ -91,13 +107,17 @@ def find_matrix_list_from_gen(GF, gen_matrix):
 
 def recurr_create_matrix_permutations(flat_matrix, GF, index):
     """
-    DESC:                                                                           \n
+    DESC:                                                                           
     generate all possible matrix permutations given a flatend matrix with know and
-    to be determined values                                                         \n
+    to be determined values    
+
     PARAMS:             
-    flat_matrix: general matrix that has been flattened                             \n
-    GF:          The field which our general matrix is allowed to span              \n
-    index:       The current index that is being split on currently                 \n
+    flat_matrix(list of int): general matrix that has been flattened                             
+    GF(int): The field which our general matrix is allowed to span              
+    index(int): The current index that is being split on currently    
+
+    RETURN:
+    output_perm_list(list of matrix of int): current list of all matrices reachable from the generalized matrix             
     """
     if index >= len(flat_matrix):
         # we have reached the end of the list, starting return process
@@ -131,11 +151,38 @@ def recurr_create_matrix_permutations(flat_matrix, GF, index):
                     output_perm_list.append(np_arr)
         return output_perm_list
 
-
-if __name__ == "__simpleCholSqrtGenerator__":
-    GF = 0
-    n = 0 
+def main():
+    """Driver for program
+    Command to run: python treeCholSqrtGenerator.py runtype GF n
+    runtype = either "chol" or "sqrt"
+    GF = any integer >= 0
+    n = any integer >= 0"""
+    runtype = sys.argv[1]
+    GF = int(sys.argv[2])
+    n = int(sys.argv[3])
     
-    GF = input("what do you want your GF to be(int): ")
-    n = input("what do you want your n to be(int): ")
-    main(None, GF, n)
+    print("you entered \nruntype: " + runtype + "\nGF: " + str(GF) + "\nn: " + str(n))
+
+    if len(sys.argv) != 4:
+        print("error, incorrect amount of inputs")
+        return
+    if type(GF) is not int or GF < 0:
+        print("error, GF is wrong")
+        return
+    if type(n) is not int or n < 0:
+        print("error, n is wrong")
+        return
+    
+
+    if runtype == "chol":
+        generate_list_of_type_matrices(GF, n, runtype)
+    elif runtype == "sqrt":
+        generate_list_of_type_matrices(GF, n, runtype)
+    else:
+        print("error, unreadable runtype")
+        return
+    
+
+if __name__ == "__main__":
+    print("Starting simpleCholSqrtGenerator")
+    main()
